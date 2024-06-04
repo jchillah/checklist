98ae234ff6b0bbfc97a3f64309be31d273f87845
David Sent <david.sent@app-akademie.com>
2024-06-04 15:05:59 +0200

    impl checklist

---
+++ lib/src/features/checklist/domain/check_item.dart
@@ -0,0 +1,11 @@
+class CheckItem {
+  // Attribute
+  final String title;
+  final DateTime timestamp;
+
+  // Konstruktor
+  const CheckItem({required this.title, required this.timestamp});
+
+  // Methoden
+  // ...
+}
--- lib/src/features/checklist/presentation/checklist_screen.dart
+++ lib/src/features/checklist/presentation/checklist_screen.dart
@@ -1,15 +1,77 @@
+import 'package:checklist/src/features/checklist/domain/check_item.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class ChecklistScreen extends StatelessWidget {
+class ChecklistScreen extends StatefulWidget {
   const ChecklistScreen({super.key});
+
+  @override
+  State<ChecklistScreen> createState() => _ChecklistScreenState();
+}
+
+class _ChecklistScreenState extends State<ChecklistScreen> {
+  // State
+  final List<CheckItem> _checkList = [];
+  final TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text('Hallo Welt'),
-        ],
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: _checkList.length,
+                itemBuilder: (context, index) {
+                  final currentItem = _checkList[index];
+                  return Card(
+                    child: ListTile(
+                      title: Text(currentItem.title),
+                      subtitle: Text(
+                          DateFormat('dd.MM.yy').format(currentItem.timestamp)),
+                      trailing: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _checkList.remove(currentItem);
+                          });
+                        },
+                        icon: const Icon(Icons.delete),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextField(controller: _controller),
+            const SizedBox(height: 16),
+            FilledButton(
+              onPressed: () {
+                final checkItem = CheckItem(
+                  title: _controller.text,
+                  timestamp: DateTime.now(),
+                );
+                // Textfeld leeren
+                _controller.clear();
+
+                // zur Liste adden und build-Methode neu ausführen
+                setState(() {
+                  _checkList.add(checkItem);
+                });
+              },
+              child: const Text("Hinzufügen"),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
